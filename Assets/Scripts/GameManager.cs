78ed9a04bef6b5dfa03c7c36a05a5a011530545a
Acil Djehiche <acil.djehiche@outlook.com>
2025-10-16 09:25:41 +0100

    Created title screen and added a new main scene with player movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI titleText;
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+        startButton.gameObject.SetActive(false);
+        titleText.gameObject.SetActive(false);
+    }
+}
