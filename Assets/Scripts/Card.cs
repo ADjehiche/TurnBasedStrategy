92d371de20206d1103adee181cdb4b9e7eb83621
KhaledAlmazeedi <KhaledAlmazeedi@outlook.com>
2025-10-19 22:26:22 +0100

    Setup Card PreFab
    I created a cardprefab to display CardData then I will start working on a script

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CardGame
+{
+    public class CardSystem : ScriptableObject
+    {
+        public string cardName;
+        public CardType cardType;
+        public int health;
+
+        public int damageMin;
+        public int damageMax;
+        public DamageType damageType;
+
+        public enum CardType
+        {
+            melee,
+            ranged,
+            spell,
+            defense,
+
+        }
+        public enum DamageType
+        {
+            melee,
+            ranged,
+            spell,
+            defense,
+        }
+
+    }
+}
---
+++ Assets/Scripts/CardSystem.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CardGame
+{
+    [CreateAssetMenu(fileName = "New Card", menuName = "Card")]
+    public class Card : ScriptableObject
+    {
+        public string cardName;
+        public List<CardType> cardType;
+        public int health;
+
+        public int damageMin;
+        public int damageMax;
+        public List<DamageType> damageType;
+
+        public enum CardType
+        {
+            melee,
+            ranged,
+            spell,
+            defense,
+
+        }
+        public enum DamageType
+        {
+            melee,
+            ranged,
+            spell,
+            defense,
+        }
+
+    }
+}
